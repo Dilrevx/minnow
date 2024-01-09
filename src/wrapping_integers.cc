67f603528362ed55710816e6b13ae8d211ccac50
@ <@>
2024-01-10 00:44:08 +0800

    [fix] UINT32_MAX -> UINT32_MAX + 1

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -18,10 +18,10 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
     return init;
 
   // checkpoint > init
-  uint64_t possible_value = init + ( ( checkpoint - init ) / UINT32_MAX ) * UINT32_MAX;
+  uint64_t possible_value = init + ( ( checkpoint - init ) / ( UINT32_MAX + 1ull ) ) * ( UINT32_MAX + 1ull );
 
-  if ( checkpoint - possible_value <= possible_value + UINT32_MAX - checkpoint )
+  if ( checkpoint - possible_value <= possible_value + UINT32_MAX + 1ull - checkpoint )
     return possible_value;
   else
-    return possible_value + UINT32_MAX;
+    return possible_value + UINT32_MAX + 1ull;
 }
