6458cd64712c289781b606dab8570883cc958a17
@ <@>
2024-01-17 08:18:54 +0800

    debug, reference lost. chksum, 0-mask

--- src/router.cc
+++ src/router.cc
@@ -28,23 +28,22 @@ void Router::route()
 {
   do {
     for ( auto& interface_ : interfaces_ ) {
-      auto opt = interface_.maybe_receive();
-      while ( opt.has_value() ) {
+      for ( auto opt = interface_.maybe_receive(); opt.has_value(); opt = interface_.maybe_receive() ) {
         auto& dgram = opt.value();
         auto& header = dgram.header;
+        if ( header.ttl == 0 || --header.ttl == 0 )
+          continue;
+        header.compute_checksum();
 
         auto ip = header.dst;
-
         auto rtentry = match_rt_entry( ip );
-
         // no match route
         if ( not rtentry.has_value() )
           continue;
-        auto [next_hop, if_n] = rtentry.value();
-        auto dstif = interface( if_n );
-        dstif.send_datagram( dgram, next_hop );
 
-        opt = interface_.maybe_receive();
+        auto [next_hop, if_n] = rtentry.value();
+        auto& dstif = interface( if_n );
+        dstif.send_datagram( dgram, next_hop );
       }
     }
   } while ( false );
@@ -54,11 +53,11 @@ void Router::route()
 optional<pair<Address, size_t>> Router::match_rt_entry( const uint32_t ip )
 {
   constexpr auto getMask = []( const uint32_t _ip, const uint8_t mask ) {
-    uint32_t bits = numeric_limits<uint32_t>::max() << ( 32 - mask );
+    uint64_t bits = numeric_limits<uint32_t>::max() * 1ULL << ( 32 - mask );
     return _ip & bits;
   };
 
-  int maxLen = 0;
+  int maxLen = -1;
   const optional<Address>* p1 = nullptr;
   const size_t* p2 = nullptr;
   for ( const auto& [pref, mask, nhop, num] : route_table ) {
